fc34564765af26b4e9d5277b759d7b6b1ee3dda6
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-07 19:22:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+var multiply = (num1, num2) => {
+    return num1 * num2;
+  };
+multiply(31, 2);
+
+var random =Math.floor(Math.random() * 10) + 1;
+var max=Math.max(1, 2, 3, 4, 5,20);
